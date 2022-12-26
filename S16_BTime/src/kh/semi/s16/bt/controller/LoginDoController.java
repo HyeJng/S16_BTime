e94c6a71df8688b0b5f2f1fe6c78c1e55ccc8fa3
HyeJng <hhjngmys@gmail.com>
2022-12-26 18:18:59 +0900

    login/logout controller 코드 추가

--- S16_BTime/src/kh/semi/s16/bt/controller/LoginDoController.java
+++ S16_BTime/src/kh/semi/s16/bt/controller/LoginDoController.java
@@ -1,14 +1,14 @@
 package kh.semi.s16.bt.controller;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
-
-import org.json.simple.JSONObject;
 
 import kh.semi.s16.bt.model.MemberVo;
 import kh.semi.s16.bt.service.MemberService;
@@ -33,14 +33,18 @@ public class LoginDoController extends HttpServlet {
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		response.setContentType("application/json;charset=UTF-8");
 		MemberService service = new MemberService();
+		PrintWriter out = response.getWriter();
+		
 		String id = request.getParameter("id");
 		String pwd = request.getParameter("pwd");
-		JSONObject job = new JSONObject();
 		
 		MemberVo m = service.login(id, pwd);
 		if(m != null) {
 			HttpSession session = request.getSession();
 			session.setAttribute("member", m);
+			out.append("success");
+		}else {
+			out.append("fail");
 		}
 		//TODO
 	}
